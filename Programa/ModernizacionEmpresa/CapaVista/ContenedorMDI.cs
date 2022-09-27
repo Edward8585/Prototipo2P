0d66d9820f718dcae9c05a706f3aa5bbf73d5cc4
Edward8585 <ewetz2014ig@gmail.com>
2022-09-27 14:00:13 -0600

    Se agrega todo el parcial

---
+++ Programa/ModernizacionEmpresa/CapaVista/ContenedorMDI.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVista
+{
+    public partial class ContenedorMDI : Form
+    {
+        private Ventas formaV;
+        private Inventarios formaI;
+        public ContenedorMDI()
+        {
+            InitializeComponent();
+        }
+
+        private void ventasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+        void CerrarForma(object sender, FormClosedEventArgs e)
+        {
+            formaV = null;
+            formaI = null;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inventariosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ventasToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //Validacion para no abrir mas de una ventana del mismo form
+            if (formaV == null)
+            {
+                formaV = new Ventas();
+                formaV.MdiParent = this;
+                formaV.FormClosed += new FormClosedEventHandler(CerrarForma);
+                formaV.Show();
+            }
+            else
+            {
+                formaV.Activate();
+            }
+        }
+
+        private void inventariosToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //Validacion para no abrir mas de una ventana del mismo form
+            if (formaV == null)
+            {
+                formaI = new Inventarios();
+                formaI.MdiParent = this;
+                formaI.FormClosed += new FormClosedEventHandler(CerrarForma);
+                formaI.Show();
+            }
+            else
+            {
+                formaV.Activate();
+            }
+        }
+    }
+}
